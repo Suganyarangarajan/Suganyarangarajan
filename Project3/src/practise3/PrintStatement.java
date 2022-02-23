173d337ca266f3cdf3fe2e1947b304ba48b0ecae
mmani <mmani@54VMJR2>
2022-02-23 17:16:53 -0500

    Print statement updated

---
+++ Project3/src/practise3/PrintStatement.java
@@ -0,0 +1,10 @@
+package practise3;
+
+public class PrintStatement {
+
+	public static void main(String[] args) {
+System.out.println("Welcome to the world of java");
+System.out.println(5+5);
+
+	}
+}
