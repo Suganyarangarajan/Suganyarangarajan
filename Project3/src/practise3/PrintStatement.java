cb55f639747c02eeb236ba733ffd3d94016f6b07
mmani <mmani@54VMJR2>
2022-02-23 17:27:49 -0500

    Added a line

--- Project3/src/practise3/PrintStatement.java
+++ Project3/src/practise3/PrintStatement.java
@@ -3,6 +3,7 @@ package practise3;
 public class PrintStatement {
 
 	public static void main(String[] args) {
+		System.out.println("Print your statement here");
 System.out.println("Welcome to the world of java");
 System.out.println(5+5);
 
